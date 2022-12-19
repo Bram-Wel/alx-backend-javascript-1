a5d9cd4601a2bc371b34e81ea7cb314abae5b7fb
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-19 17:29:34 +0300

    100-createIteratorObject

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,7 @@
 export default function createIteratorObject(report) {
-  let items = []
-  Object.values(report.allEmployees).forEach(item => items = [...items, ...item]);
+  let items = [];
+  Object.values(report.allEmployees).forEach((item) => {
+    items = [...items, ...item];
+  });
   return items[Symbol.iterator]();
 }
