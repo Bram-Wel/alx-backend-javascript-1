f7ff21b6d03c421b61ed5022a8ec2fbb9aaf2557
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-21 08:05:36 +0300

    3-all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  uploadPhoto().then((res) => {
-    const profile = {};
-    profile.photo = res.body;
-
-    createUser().then((res) => {
-      console.log(Object.values({ ...profile, ...res }).join(' '));
-    }).catch(() => console.log('Signup system offline'));
-  }).catch(() => console.log('Signup system offline'));
+export default async function handleProfileSignup() {
+  try {
+    const { body: photo } = await uploadPhoto();
+    const { firstName, lastName } = await createUser();
+    console.log(`${photo} ${firstName} ${lastName}`);
+  } catch (err) {
+    console.log('Signup system offline');
+  }
 }
