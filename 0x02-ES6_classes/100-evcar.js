ae25a7d09e4090c18d1bb93a38907b6edf0f5782
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-22 10:24:35 +0300

    100-evcar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,21 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(range) {
+    if (typeof (range) !== 'string') throw new TypeError('Range must be a string');
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new super.constructor();
+  }
+}
