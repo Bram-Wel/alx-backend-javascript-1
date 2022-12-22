b52f24ae01cf614bf66c4f46be584be3d81c28fc
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-22 15:39:48 +0300

    4-pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,31 +1,41 @@
 import Currency from './3-currency';
 
-export default class Pricing {
+export default class pricing {
   constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
     this._amount = amount;
     this._currency = currency;
+  }
+
+  set amount(value) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = value;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(amount) {
-    if (typeof (amount) !== 'number') throw new TypeError('Amount must be a number');
-    this._amount = amount;
-  }
-
   get currency() {
     return this._currency;
   }
 
-  set currency(currency) {
-    if (!(currency instanceof Currency)) throw new TypeError('Currency must be an instance of Currency');
-    this._currency = currency;
-  }
-
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} ${this._currency.code}`;
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
