552b94adb852eebc6a97ee59327fbacf0527914a
Ernest Wambua <ernestwambua2@gmail.com>
2023-01-03 12:57:05 +0300

    8-clean_set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
-  let string = '';
-  for (const item of set) {
-    if (item.startsWith(startString)) {
-      string += `${item.slice(startString.length)}-`;
-    }
-  }
-  return string.slice(0, string.length - 1);
+  let result = '';
+  if (!startString || !startString.length) return result;
+  set.forEach((i) => {
+  if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
+  });
+  return result.slice(0, result.length - 1);
 }
