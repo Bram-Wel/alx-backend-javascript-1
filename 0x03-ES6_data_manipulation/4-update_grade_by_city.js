0bfe759697a8d774a84551c4fa1623cd0aee024e
Ernest Wambua <ernestwambua2@gmail.com>
2023-01-03 08:14:02 +0300

    4-update_grade_by_city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,6 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city).map((student) => {
+    const grade = newGrades.filter((grade) => grade.studentId === student.id)[0];
+    return { ...student, grade: grade ? grade.grade : 'N/A' };
+  });
+}
