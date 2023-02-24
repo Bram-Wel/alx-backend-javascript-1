5a68414cc3537c6a586c4f903db4f5c502b20e58
Ernest Wambua <ernestwambua2@gmail.com>
2023-02-24 07:34:56 +0300

    assert

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -2,8 +2,8 @@
  * The function should round a and b and return the sum of it
  */
 
-function calculateNumber(a, b) {
-	return Math.round(a) + Math.round(b);
+function calculateNumber (a, b) {
+  return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
